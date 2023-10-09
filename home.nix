206b4edabe8528e57369579dab6d48003e68ac83
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-10-09 12:18:12 +0500

    added git configs

--- home.nix
+++ home.nix
@@ -343,4 +343,25 @@
       };
     };
   };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+
+    # User credentials
+    userName = "Sokhibjon Orzikulov";
+    userEmail = "sakhib@orzklv.uz";
+
+    # GPG Signing
+    signing = {
+      signByDefault = true;
+      key = "00D27BC687070683FBB9137C3C35D3AF0DA1D6A8";
+    };
+
+    # Git ignores
+    ignores = [
+      ".idea"
+      ".DS_Store"
+    ];
+  };
 }
