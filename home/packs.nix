8256cc9f3aa6e4262134e48259526eb2e1d115b7
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-10-11 15:48:55 +0500

    removing compiler libraries

--- home/packs.nix
+++ home/packs.nix
@@ -8,24 +8,15 @@
       pkgs.ispell
 
       # Developer Mode
-      pkgs.automake
-      pkgs.autoconf
       pkgs.cargo-update
-      pkgs.clang-tools
-      pkgs.cmake
-      pkgs.cocoapods
-      pkgs.gcc
       pkgs.gh
       pkgs.git-lfs
       pkgs.gitoxide
       pkgs.gitui
-      pkgs.libgccjit
       pkgs.libheif
-      pkgs.llvm
       pkgs.just
       pkgs.gnumake
       pkgs.mdbook
-      pkgs.pcre
       pkgs.shfmt
       pkgs.wget
 
