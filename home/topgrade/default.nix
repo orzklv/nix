3a028bcc0260e7c32ba886faeb775c4a880e8a1a
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-12-01 10:53:15 +0500

    modified nix upgrade commands

--- home/topgrade/default.nix
+++ home/topgrade/default.nix
@@ -5,11 +5,17 @@
       misc = {
         assume_yes = true;
         disable = [
+          "nix"
           "bun"
           "node"
           "pnpm"
+          "yarn"
           "home_manager"
         ];
+      };
+      commands = {
+        "Nix" = "nix-channel --update && nix-env -u '*'";
+        "Home Manager" = "home-manager switch --flake github:orzklv/nix";
       };
       brew = {
         autoremove = true;
