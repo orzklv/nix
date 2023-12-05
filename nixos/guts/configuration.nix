05b1927f804056d9c89b0e4c3c168a2143a4148c
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-12-05 10:52:52 +0500

    fixed path to home

--- nixos/guts/configuration.nix
+++ nixos/guts/configuration.nix
@@ -181,7 +181,7 @@
     extraSpecialArgs = { inherit inputs outputs; };
     users = {
       # Import your home-manager configuration
-      sakhib = import ../home/nixos.nix;
+      sakhib = import ../../home/nixos.nix;
     };
   };
 
