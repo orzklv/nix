54cfb0de654c5028c5c7b04d3c2b7049594f7a50
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-12-06 14:30:18 +0500

    adding nginx module

---
+++ modules/nixos/nginx/default.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Enable the Nginx web server
+  services.nginx.enable = true;
+
+  # Configure Nginx
+  services.nginx = {
+    # Define a simple virtual host
+    virtualHosts."localhost" = {
+      # Enable this virtual host
+      enable = true;
+
+      # Specify the root directory for the website
+      root = "/var/www/localhost";
+
+      # Basic configuration for serving static files
+      locations."/" = {
+        extraConfig = ''
+          index index.html;
+        '';
+      };
+    };
+  };
+
+  # Ensure the firewall allows HTTP and HTTPS traffic
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  # The following line is optional and only necessary if you are using Let's Encrypt
+  # for SSL certificates
+  # services.nginx.recommendedGzipSettings = true;
+
+  # Additional system-wide settings can go here
+};
--- nixos/griffith/configuration.nix
+++ nixos/griffith/configuration.nix
@@ -11,6 +11,7 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
     # outputs.nixosModules.example
+    outputs.nixosModules.nginx
 
     # Or modules from other flakes (such as nixos-hardware):
     # inputs.hardware.nixosModules.common-cpu-amd
