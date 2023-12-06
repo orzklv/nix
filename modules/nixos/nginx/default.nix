c54664f471ac61f9421dfdd1650cc1820d020c58
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-12-06 17:59:01 +0500

    Update default.nix

--- modules/nixos/nginx/default.nix
+++ modules/nixos/nginx/default.nix
@@ -1,5 +1,25 @@
 { config, lib, pkgs, ... }:
 {
+  # SSL Certification Configs
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "cctld@uzinfocom.uz";
+  };
+
+  security.acme.certs."localhost" = {
+    webroot = "/var/lib/acme/.challenges";
+    email = "cctld@uzinfocom.uz";
+    # Ensure that the web server you use can read the generated certs
+    # Take a look at the group option for the web server you choose.
+    group = "nginx";
+    # Since we have a wildcard vhost to handle port 80,
+    # we can generate certs for anything!
+    # Just make sure your DNS resolves them.
+    extraDomainNames = [ "mail.localhost" ];
+  };
+
+  users.users.nginx.extraGroups = [ "acme" ];
+
   # Configure Nginx
   services.nginx = {
     # Enable the Nginx web server
@@ -12,6 +32,11 @@
         enableACME = true;
         forceSSL = true;
 
+        serverAliases = [ 
+          "172.16.9.179"
+          "192.168.0.2"
+        ];
+
         # Specify the root directory for the website
         root = "/var/www/localhost";
 
@@ -20,6 +45,16 @@
           extraConfig = ''
             index index.html;
           '';
+        };
+      };
+
+      "acmechallenge.localhost" = {
+        # Catchall vhost, will redirect users to HTTPS for all vhosts
+        locations."/.well-known/acme-challenge" = {
+          root = "/var/lib/acme/.challenges";
+        };
+        locations."/" = {
+          return = "301 https://$host$request_uri";
         };
       };
     };
