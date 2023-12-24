86f929cc7cefa35de14ce9c8e71ef379b2b5b715
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-12-24 15:24:49 +0500

    changed neofetch command

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -26,6 +26,7 @@
       "...." = "cd ../..";
       "celar" = "clear";
       ":q" = "exit";
+      neofetch = "neofetch --source ~/.config/neofetch/logo.ascii";
       ssh-hosts = "grep -P \"^Host ([^*]+)$\" $HOME/.ssh/config | sed 's/Host //'";
 
       # Polite motherfucker!
