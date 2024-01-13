1b02f572bcf8e78f19c6c389310cd128dcd3f729
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-01-14 00:20:49 +0500

    Update default.nix

--- modules/nixos/media/default.nix
+++ modules/nixos/media/default.nix
@@ -1,8 +1,9 @@
-{config, ...}: {
-  home-manager.users = {
-    # Media folder to symlink /media
-    sakhib.file."Media" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/media";
-    };
+{config, lib, ...}: {
+  config = {
+    system.activationScripts.createMediaSymlink = lib.stringAfter [ "users" ] ''
+    if [ "${config.users.users.sakhib.isNormalUser}" = "1" ]; then
+      ln -sfn /media ${config.users.users.sakhib.home}/Media
+    fi
+    ''; 
   };
 }
