3f646cf53d6949666a5617b5535bcb1cfb111202
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-04-28 00:30:50 -0700

    adding bootloader theme

--- modules/nixos/bootloader/default.nix
+++ modules/nixos/bootloader/default.nix
@@ -1,25 +1,31 @@
 {pkgs, ...}: {
   config = {
     # Bootloader.
-    boot.loader = {
-      # systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      grub = {
-        enable = true;
-        devices = ["nodev"];
-        efiSupport = true;
-        useOSProber = true;
-        theme = pkgs.stdenv.mkDerivation {
-          pname = "distro-grub-themes";
-          version = "3.1";
-          src = pkgs.fetchFromGitHub {
-            owner = "AdisonCavani";
-            repo = "distro-grub-themes";
-            rev = "v3.1";
-            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+    boot = {
+      loader = {
+        # systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+        grub = {
+          enable = true;
+          devices = ["nodev"];
+          efiSupport = true;
+          useOSProber = true;
+          theme = pkgs.stdenv.mkDerivation {
+            pname = "distro-grub-themes";
+            version = "3.1";
+            src = pkgs.fetchFromGitHub {
+              owner = "AdisonCavani";
+              repo = "distro-grub-themes";
+              rev = "v3.1";
+              hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+            };
+            installPhase = "cp -r customize/nixos $out";
           };
-          installPhase = "cp -r customize/nixos $out";
         };
+      };
+      plymouth = {
+        enable = true;
+        theme = "breeze";
       };
     };
   };
