b49124e37cf1131fddfc99ee432dcf2e54774437
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-05-04 15:21:21 +0500

    comments for autologin

--- nixos/parallels/configuration.nix
+++ nixos/parallels/configuration.nix
@@ -88,6 +88,7 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Autologin user sakhib
   services.xserver.displayManager.autoLogin = {
     enable = true;
     user = "sakhib";
