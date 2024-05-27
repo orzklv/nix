99e28d4ac5811695ae39dda7304c8b90ca66a877
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-05-28 01:59:49 +0500

    check whether does /media exists

--- modules/nixos/media/default.nix
+++ modules/nixos/media/default.nix
@@ -9,12 +9,16 @@
     # fi
 
     # loop /home every user folder and create symlink to /media folder as Media, e.g: /home/sakhib/Media
-    for user in /home/*; do
-      if [ -d /media ]; then
-        ln -sfn /media $user/Media
-        chown -R :users $user/Media
-        chmod -R 777 $user/Media
-      fi  
-    done
+    if [ -d /media ]; then
+      for user in /home/*; do
+        if [ -d /media ]; then
+          ln -sfn /media $user/Media
+          chown -R :users $user/Media
+          chmod -R 777 $user/Media
+        fi  
+      done
+    else
+      echo "Media folder not found"
+    fi
   '';
 }
