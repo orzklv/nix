8423daee774dc1678ab59bd561d4a18f73e315f6
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-05-30 04:39:28 +0500

    removed options + ported configs for every zone

--- modules/server/bind/kolyma-1.nix
+++ modules/server/bind/kolyma-1.nix
@@ -7,17 +7,13 @@
   services.bind = {
     enable = true;
     directory = "/var/bind";
-    extraConfig = ''
-      options {
-        allow-transfer { 65.109.61.35; }; # IP address of the slave server ns2.kolyma.uz
-      };
-    '';
 
     zones = {
       "dumba.uz" = {
         master = true;
         name = "dumba.uz";
         file = "/var/dns/dumba.uz.zone";
+        slaves = ["65.109.61.35"];
       };
     };
   };
