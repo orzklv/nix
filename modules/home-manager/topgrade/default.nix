36dac674d53732c339d3b76e8b58e4dcd11e4072
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-05-29 23:18:22 +0500

    nix compatible topgrade config

--- modules/home-manager/topgrade/default.nix
+++ modules/home-manager/topgrade/default.nix
@@ -3,21 +3,26 @@
     enable = true;
     settings = {
       misc = {
-        assume_yes = true;
         disable = [
-          "nix"
           "bun"
           "node"
           "pnpm"
           "yarn"
           "cargo"
-          "home_manager"
           "vscode"
         ];
+        no_retry = true;
+        assume_yes = true;
+        no_self_update = true;   
       };
       commands = {
-        "Nix" = "nix-channel --update && nix-env -u '*'";
-        "Home Manager" = "home-manager switch --flake github:orzklv/nix";
+        # "Nix" = "nix-channel --update && nix-env -u '*'";
+        # "NixOS" = "sudo nixos-rebuild switch";
+        # "Home Manager" = "home-manager switch --flake github:orzklv/nix";
+      };
+      linux = {
+        nix_arguments = ["--flake" "github:orzklv/nix"];
+        home_manager_arguments = ["--flake" "github:orzklv/nix"];
       };
       brew = {
         autoremove = true;
