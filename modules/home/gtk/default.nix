74cf8bffc29819d27748e7b875f3f51f7936a3eb
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-06-30 20:41:40 +0500

    setting theme for legacy

--- modules/home/gtk/default.nix
+++ modules/home/gtk/default.nix
@@ -43,6 +43,10 @@
         color-scheme = "prefer-dark";
       };
 
+      "/org/gnome/shell/extensions/user-theme" = {
+        name = "Nordic-standard-buttons";
+      };
+
       "org/gnome/desktop/background" = {
         picture-uri = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
         picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
