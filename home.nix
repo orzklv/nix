0e01d27f3a459b48a2646b998c7496a359d0d255
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-10-01 14:35:51 +0500

    new home approach but it's working

--- home.nix
+++ home.nix
@@ -18,31 +18,29 @@ let
   ];
 
   osx =
-    builtins.elemAt [
+    builtins.elem pkgs.system [
       "aarch64-darwin"
       "x86_64-darwin"
-    ]
-      (pkgs.system);
+    ];
 
   home =
     if osx
     then "Users"
     else "home";
 
-  macos =
-    if osx
-    then {
-      # This is to ensure programs are using ~/.config rather than
-      # /Users/sakhib/Library/whatever
-      xdg.enable = true;
-    }
-    else {
-      # This is required information for home-manager to do its job
-      home = {
-        # Don't check if home manager is same as nixpkgs
-        enableNixpkgsReleaseCheck = false;
-      };
+  macos = lib.mkIf osx {
+    # This is to ensure programs are using ~/.config rather than
+    # /Users/sakhib/Library/whatever
+    xdg.enable = true;
+  };
+
+  linux = lib.mkIf (!osx) {
+    # This is required information for home-manager to do its job
+    home = {
+      # Don't check if home manager is same as nixpkgs
+      enableNixpkgsReleaseCheck = false;
     };
+  };
 
   cfg = {
     # This is required information for home-manager to do its job
@@ -54,12 +52,12 @@ let
       # Tell it to map everything in the `config` directory in this
       # repository to the `.config` in my home-manager directory
       file.".config" = {
-        source = ../configs/config;
+        source = ./configs/config;
         recursive = true;
       };
 
       file.".local/share" = {
-        source = ../configs/share;
+        source = ./configs/share;
         recursive = true;
       };
     };
@@ -68,7 +66,12 @@ let
     programs.home-manager.enable = true;
   };
 in
-lib.mkMerge ([
-  cfg
-  macos
-] ++ modules)
+{
+  imports = modules;
+
+  config = lib.mkMerge [
+    cfg
+    macos
+    linux
+  ];
+}
