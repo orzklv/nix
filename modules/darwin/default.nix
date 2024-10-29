7395f88b1915eb9842f1b02dacaa9793a84a4223
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-10-30 04:48:28 +0500

    fix: import, not a relative path

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -2,5 +2,5 @@
 # These should be stuff you would like to share with others, not your personal configurations.
 {
   # List your module files here
-  users = ./users;
+  users = import ./users;
 }
--- modules/home/topgrade/default.nix
+++ modules/home/topgrade/default.nix
@@ -1,34 +1,27 @@
 { pkgs, ... }:
-let
-  # Don't update home manager on NixOS
-  duhm =
-    if pkgs.stdenv.isDarwin
-    then [ ]
-    else [ "home_manager" ];
-
-  # Disable updates for these programs
-  dull = [
-    "bun"
-    "nix"
-    "node"
-    "pnpm"
-    "yarn"
-    "cargo"
-    "vscode"
-  ];
-in
 {
   config = {
     programs.topgrade = {
       enable = true;
       settings = {
         misc = {
-          disable = dull ++ duhm;
+          disable = [
+            "bun"
+            "nix"
+            "node"
+            "pnpm"
+            "yarn"
+            "cargo"
+            "vscode"
+            "home_manager"
+          ];
           no_retry = true;
           assume_yes = true;
           no_self_update = true;
         };
-        commands = { };
+        commands = {
+          "Darwin Nix" = "darwin-rebuild switch --flake github:orzklv/nix";
+        };
         linux = {
           nix_arguments = "--flake github:orzklv/nix";
           home_manager_arguments = [ "--flake" "github:orzklv/nix" ];
