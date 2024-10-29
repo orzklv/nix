00a270a37680be61985fa3508931585c87892874
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-10-30 04:55:08 +0500

    chore:  tuned topgrade's behavior

--- modules/home/topgrade/default.nix
+++ modules/home/topgrade/default.nix
@@ -15,12 +15,13 @@
             "vscode"
             "home_manager"
           ];
+          pre_sudo = true;
           no_retry = true;
           assume_yes = true;
           no_self_update = true;
         };
         commands = {
-          "Darwin Nix" = "darwin-rebuild switch --flake github:orzklv/nix";
+          "Darwin Nix" = "sudo darwin-rebuild switch --flake github:orzklv/nix";
         };
         linux = {
           nix_arguments = "--flake github:orzklv/nix";
