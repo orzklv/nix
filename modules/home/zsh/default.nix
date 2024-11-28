78af015c3d410e505f699304c210b2ec777a52ab
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-11-28 23:02:48 +0500

    chore: zellij autostart

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -115,6 +115,18 @@ let
 
     ${mac-extra}
 
+    if [[ -z "$ZELLIJ" ]]; then
+        if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+            zellij attach -c
+        else
+            zellij
+        fi
+
+        if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+            exit
+        fi
+    fi
+
     # Extra services
     # here...
   '';
