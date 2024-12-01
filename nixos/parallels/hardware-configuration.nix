3965bbdaf07a2caf3d222b6714252b421fbcbdbd
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-12-01 13:10:30 +0500

    chore: remove not working config

--- nixos/parallels/hardware-configuration.nix
+++ nixos/parallels/hardware-configuration.nix
@@ -27,16 +27,6 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  # fileSystems."/media/psf/RosettaLinux" = {
-  #   device = "RosettaLinux";
-  #   fsType = "prl_fs";
-  # };
-
-  # fileSystems."/media/psf/iCloud" = {
-  #   device = "iCloud";
-  #   fsType = "prl_fs";
-  # };
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
