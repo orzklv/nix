1682f5b3c09f8af1a433df59d5be72fb4945640c
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-02-09 10:37:53 +0500

    chore: don't hide system messages

--- modules/nixos/boot/grub.nix
+++ modules/nixos/boot/grub.nix
@@ -7,9 +7,9 @@
       kernelParams = [
         "quiet"
         "splash"
-        "rd.systemd.show_status=false"
-        "rd.udev.log_level=3"
-        "udev.log_priority=3"
+        # "rd.systemd.show_status=false"
+        # "rd.udev.log_level=3"
+        # "udev.log_priority=3"
       ];
       loader = {
         efi.canTouchEfiVariables = true;
