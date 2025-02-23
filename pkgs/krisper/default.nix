caf7b55ae5d86d150086262f6b2ae35aa19521d8
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-02-24 04:42:57 +0500

    fix: meta for patcher too

--- pkgs/krisper/default.nix
+++ pkgs/krisper/default.nix
@@ -10,26 +10,32 @@
   xdg-utils,
 }: let
   krisp-patcher =
-    writers.writePython3Bin "krisp-patcher"
-    {
-      libraries = with python3Packages; [
-        capstone
-        pyelftools
-      ];
-      flakeIgnore = [
-        "E501" # line too long (82 > 79 characters)
-        "F403" # 'from module import *' used; unable to detect undefined names
-        "F405" # name may be undefined, or defined from star imports: module
-      ];
-    }
-    (
-      builtins.readFile (
-        fetchurl {
-          url = "https://raw.githubusercontent.com/sersorrel/sys/afc85e6b249e5cd86a7bcf001b544019091b928c/hm/discord/krisp-patcher.py";
-          sha256 = "sha256-h8Jjd9ZQBjtO3xbnYuxUsDctGEMFUB5hzR/QOQ71j/E=";
-        }
-      )
-    );
+    (writers.writePython3Bin "krisp-patcher"
+      {
+        libraries = with python3Packages; [
+          capstone
+          pyelftools
+        ];
+        flakeIgnore = [
+          "E501" # line too long (82 > 79 characters)
+          "F403" # 'from module import *' used; unable to detect undefined names
+          "F405" # name may be undefined, or defined from star imports: module
+        ];
+      }
+      (
+        builtins.readFile (
+          fetchurl {
+            url = "https://raw.githubusercontent.com/sersorrel/sys/afc85e6b249e5cd86a7bcf001b544019091b928c/hm/discord/krisp-patcher.py";
+            sha256 = "sha256-h8Jjd9ZQBjtO3xbnYuxUsDctGEMFUB5hzR/QOQ71j/E=";
+          }
+        )
+      ))
+    // {
+      meta = with lib; {
+        licenses = licenses.mit;
+        platforms = platforms.all;
+      };
+    };
 in
   (writeShellApplication {
     name = "krisper";
