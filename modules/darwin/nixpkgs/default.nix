831f5bf5a83e5e8979819780a0638090b80ef216
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-03-03 04:39:10 +0500

    chore: use nixos cache first + signed keys

--- modules/darwin/nixpkgs/default.nix
+++ modules/darwin/nixpkgs/default.nix
@@ -54,10 +54,11 @@
         # Enable flakes and new 'nix' command
         experimental-features = "nix-command flakes";
         substituters = [
-          "https://cache.xinux.uz/"
           "https://cache.nixos.org/"
+          "https://cache.xinux.uz/"
         ];
         trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "cache.xinux.uz-1:gX2Z53woXiIoLANfcC/Qp7vPPKVdK1sEa8MSiRhjj/M="
         ];
       };
