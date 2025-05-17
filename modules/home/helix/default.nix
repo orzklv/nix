7b1b4900960dd489bb82f5deaebac66bd7452167
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-18 04:24:30 +0500

    fix: lighten helix'es extra packages

--- modules/home/helix/default.nix
+++ modules/home/helix/default.nix
@@ -83,63 +83,26 @@
           zls
 
           #-- nix
-          nil
           nixd
-          nixpkgs-fmt
-          nixpkgs-lint
-
-          # nixd
           statix # Lints and suggestions for the nix programming language
           deadnix # Find and remove unused code in .nix source files
           alejandra # Nix Code Formatter
-
-          #-- golang
-          go
-          gomodifytags
-          iferr # generate error handling code for go
-          impl # generate function implementation for go
-          gotools # contains tools like: godoc, goimports, etc.
-          gopls # go language server
-          delve # go debugger
-
-          # -- java
-          jdk17
-          gradle
-          maven
-          spring-boot-cli
-
-          #-- lua
-          stylua
-          lua-language-server
 
           #-- bash
           nodePackages.bash-language-server
           shellcheck
           shfmt
 
-          #-- javascript/typescript --#
-          nodePackages.nodejs
-          nodePackages.typescript
-          nodePackages.typescript-language-server
-          # HTML/CSS/JSON/ESLint language servers extracted from vscode
-          nodePackages.vscode-langservers-extracted
-          nodePackages."@tailwindcss/language-server"
-
           #-- CloudNative
-          nodePackages.dockerfile-language-server-nodejs
           emmet-ls
           jsonnet
           jsonnet-language-server
-          hadolint # Dockerfile linter
 
           #-- Others
           taplo # TOML language server / formatter / validator
           nodePackages.yaml-language-server
           sqlfluff # SQL linter
           actionlint # GitHub Actions linter
-          buf # protoc plugin for linting and formatting
-          proselint # English prose linter
-          guile # scheme language
 
           #-- Misc
           tree-sitter # common language parser/highlighter
