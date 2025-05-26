be46b4d908216c83a717ee61e9205e879991d0df
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-26 23:27:44 +0500

    chore: new deps for the ios app

--- modules/darwin/homebrew/formulae.nix
+++ modules/darwin/homebrew/formulae.nix
@@ -6,6 +6,15 @@
   apps = lib.mkIf config.homebrew.enable [
     "pkl"
     "mas"
+    "mint"
+    "sourcery"
+    "swiftformat"
+    "swiftgen"
+    "xcodegen"
+    "localazy/tools/localazy"
+    "kiliankoe/formulae/swift-outdated"
+    "git-lfs"
+    "felixherrmann/tap/swift-package-list"
   ];
 in {
   # Homebrew Formulae installations
