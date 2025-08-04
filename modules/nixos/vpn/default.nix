f54983aaf01b528c9c6400deb77bfad42f565355
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-08-04 21:53:01 +0500

    chore: more vpn servers for the bloody steam... or maybe some hanime.tv...

--- modules/nixos/vpn/default.nix
+++ modules/nixos/vpn/default.nix
@@ -1,15 +1,35 @@
 {config, ...}: {
   config = {
-    sops.secrets.ostl-configs = {
-      format = "binary";
-      sopsFile = ../../../secrets/config-ostl.hell;
+    sops.secrets = {
+      hk-ns1 = {
+        format = "binary";
+        sopsFile = ../../../secrets/hk-ns1.hell;
+      };
+      gk-ns1 = {
+        format = "binary";
+        sopsFile = ../../../secrets/gk-ns1.hell;
+      };
+      gk-ns2 = {
+        format = "binary";
+        sopsFile = ../../../secrets/gk-ns2.hell;
+      };
     };
 
     services.openvpn.servers = {
-      kolyma = {
+      hk-ns1 = {
         autoStart = false;
         updateResolvConf = true;
-        config = ''config ${config.sops.secrets.ostl-configs.path}'';
+        config = ''config ${config.sops.secrets.hk-ns1.path}'';
+      };
+      gk-ns1 = {
+        autoStart = false;
+        updateResolvConf = true;
+        config = ''config ${config.sops.secrets.gk-ns1.path}'';
+      };
+      gk-ns2 = {
+        autoStart = false;
+        updateResolvConf = true;
+        config = ''config ${config.sops.secrets.gk-ns2.path}'';
       };
     };
   };
