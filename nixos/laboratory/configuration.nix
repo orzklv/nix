c7fc83bddf36b30b9c9d9fcc5e1cfb60a65db1ce
orzklv <sakhib@orzklv.uz>
2025-08-08 17:29:22 +0500

    chore: adding more languages to myself

--- nixos/laboratory/configuration.nix
+++ nixos/laboratory/configuration.nix
@@ -69,7 +69,15 @@
   };
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+
+    i18n.supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "ru_RU.UTF-8/UTF-8"
+      "uz_UZ.UTF-8/UTF-8"
+    ];
+  };
 
   # NVIDIA driver support
   services.xserver.videoDrivers = ["nvidia"];
