ba5dd6b52fc7d462e2d5965a24b2911df048f998
orzklv <sakhib@orzklv.uz>
2025-08-08 17:48:05 +0500

    chore: fix locale check reference

--- nixos/laboratory/configuration.nix
+++ nixos/laboratory/configuration.nix
@@ -72,7 +72,7 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
 
-    i18n.supportedLocales = [
+    supportedLocales = [
       "en_US.UTF-8/UTF-8"
       "ru_RU.UTF-8/UTF-8"
       "uz_UZ.UTF-8/UTF-8"
