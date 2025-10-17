aa9a771a4f0b694c439e5b25e9df38d8fb7ba39e
Orzklv <sakhib@orzklv.uz>
2025-10-17 15:05:32 +0500

    fix: remove registry from nix config

--- modules/darwin/nixpkgs/default.nix
+++ modules/darwin/nixpkgs/default.nix
@@ -42,7 +42,7 @@
 
       # This will add each flake input as a registry
       # To make nix3 commands consistent with your flake
-      registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+      # registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
 
       # This will additionally add your inputs to the system's legacy channels
       # Making legacy nix commands consistent as well, awesome!
