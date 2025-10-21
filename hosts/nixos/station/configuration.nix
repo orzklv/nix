1e1823189763bb8c3bfa7fcaa4c9719002442ffb
orzklv <sakhib@orzklv.uz>
2025-10-21 14:17:41 +0500

    chore: change default language to uzbek

--- hosts/nixos/station/configuration.nix
+++ hosts/nixos/station/configuration.nix
@@ -65,7 +65,7 @@
 
   # Select internationalisation properties.
   i18n = {
-    defaultLocale = "en_US.UTF-8";
+    defaultLocale = "uz_UZ.UTF-8/UTF-8";
 
     supportedLocales = [
       "en_US.UTF-8/UTF-8"
