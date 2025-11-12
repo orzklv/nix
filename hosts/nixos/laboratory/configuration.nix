735b05a76edd37189d43be5d77a09f8a5aa78df4
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-11-13 02:43:44 +0500

    chore: adding vpn to services

--- hosts/nixos/laboratory/configuration.nix
+++ hosts/nixos/laboratory/configuration.nix
@@ -12,6 +12,7 @@
     # If you want to use modules your own flake exports (from modules/nixos):
     outputs.nixosModules.ssh
     outputs.nixosModules.zsh
+    outputs.nixosModules.vpn
     outputs.nixosModules.game
     outputs.nixosModules.data
     outputs.nixosModules.boot
