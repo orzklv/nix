c52b8b7c1ae9cdff82e36f0aa6d19e170570bd04
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-11-16 04:16:28 +0500

    chore: comment oxidization + flake update

--- modules/nixos/oxidize/default.nix
+++ modules/nixos/oxidize/default.nix
@@ -1,67 +1,64 @@
-{pkgs, ...}: let
-  coreutils-full-name =
-    "coreuutils-full"
-    + builtins.concatStringsSep ""
-    (builtins.genList (_: "_") (builtins.stringLength pkgs.coreutils-full.version));
-
-  coreutils-name =
-    "coreuutils"
-    + builtins.concatStringsSep ""
-    (builtins.genList (_: "_") (builtins.stringLength pkgs.coreutils.version));
-
-  findutils-name =
-    "finduutils"
-    + builtins.concatStringsSep ""
-    (builtins.genList (_: "_") (builtins.stringLength pkgs.findutils.version));
-
-  diffutils-name =
-    "diffuutils"
-    + builtins.concatStringsSep ""
-    (builtins.genList (_: "_") (builtins.stringLength pkgs.diffutils.version));
-in {
+{...}:
+# coreutils-full-name =
+#   "coreuutils-full"
+#   + builtins.concatStringsSep ""
+#   (builtins.genList (_: "_") (builtins.stringLength pkgs.coreutils-full.version));
+# coreutils-name =
+#   "coreuutils"
+#   + builtins.concatStringsSep ""
+#   (builtins.genList (_: "_") (builtins.stringLength pkgs.coreutils.version));
+# findutils-name =
+#   "finduutils"
+#   + builtins.concatStringsSep ""
+#   (builtins.genList (_: "_") (builtins.stringLength pkgs.findutils.version));
+# diffutils-name =
+#   "diffuutils"
+#   + builtins.concatStringsSep ""
+#   (builtins.genList (_: "_") (builtins.stringLength pkgs.diffutils.version));
+{
   config = {
     security.sudo-rs.enable = true;
 
-    system.replaceDependencies.replacements = [
-      # coreutils
-      {
-        # system
-        oldDependency = pkgs.coreutils-full;
-        newDependency = pkgs.symlinkJoin {
-          # Make the name length match so it builds
-          name = coreutils-full-name;
-          paths = [pkgs.uutils-coreutils-noprefix];
-        };
-      }
-      {
-        # applications
-        oldDependency = pkgs.coreutils;
-        newDependency = pkgs.symlinkJoin {
-          # Make the name length match so it builds
-          name = coreutils-name;
-          paths = [pkgs.uutils-coreutils-noprefix];
-        };
-      }
-      # findutils
-      {
-        # applications
-        oldDependency = pkgs.findutils;
-        newDependency = pkgs.symlinkJoin {
-          # Make the name length match so it builds
-          name = findutils-name;
-          paths = [pkgs.uutils-findutils];
-        };
-      }
-      # diffutils
-      {
-        # applications
-        oldDependency = pkgs.diffutils;
-        newDependency = pkgs.symlinkJoin {
-          # Make the name length match so it builds
-          name = diffutils-name;
-          paths = [pkgs.uutils-diffutils];
-        };
-      }
-    ];
+    # system.replaceDependencies.replacements = [
+    #   # coreutils
+    #   {
+    #     # system
+    #     oldDependency = pkgs.coreutils-full;
+    #     newDependency = pkgs.symlinkJoin {
+    #       # Make the name length match so it builds
+    #       name = coreutils-full-name;
+    #       paths = [pkgs.uutils-coreutils-noprefix];
+    #     };
+    #   }
+    #   {
+    #     # applications
+    #     oldDependency = pkgs.coreutils;
+    #     newDependency = pkgs.symlinkJoin {
+    #       # Make the name length match so it builds
+    #       name = coreutils-name;
+    #       paths = [pkgs.uutils-coreutils-noprefix];
+    #     };
+    #   }
+    #   # findutils
+    #   {
+    #     # applications
+    #     oldDependency = pkgs.findutils;
+    #     newDependency = pkgs.symlinkJoin {
+    #       # Make the name length match so it builds
+    #       name = findutils-name;
+    #       paths = [pkgs.uutils-findutils];
+    #     };
+    #   }
+    #   # diffutils
+    #   {
+    #     # applications
+    #     oldDependency = pkgs.diffutils;
+    #     newDependency = pkgs.symlinkJoin {
+    #       # Make the name length match so it builds
+    #       name = diffutils-name;
+    #       paths = [pkgs.uutils-diffutils];
+    #     };
+    #   }
+    # ];
   };
 }
