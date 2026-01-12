a99a7e28d45ff6c6a9ba687a14b62e4437d34fe6
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-01-13 02:15:38 +0500

    chore: arm64-parallels

---
+++ hosts/arm64-linux/parallels/configuration.nix
@@ -0,0 +1,108 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  inputs,
+  outputs,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    outputs.nixosModules.ssh
+    outputs.nixosModules.zsh
+    outputs.nixosModules.vpn
+    outputs.nixosModules.data
+    outputs.nixosModules.boot
+    outputs.nixosModules.sound
+    outputs.nixosModules.users
+    outputs.nixosModules.secret
+    outputs.nixosModules.oxidize
+    outputs.nixosModules.desktop
+    outputs.nixosModules.nixpkgs
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+
+    # Home Manager NixOS Module
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  networking = {
+    hostName = "Parallels"; # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networkmanager.enable = true;
+
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    firewall.enable = false;
+
+    # Additional hosts for work
+    hosts = {
+      "192.168.7.150" = ["ipa.uzinfocom.uz"];
+    };
+  };
+
+  # Enable bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  # Set your time zone.
+  time = {
+    timeZone = "Asia/Tashkent";
+    hardwareClockInLocalTime = true;
+  };
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "uz_UZ.UTF-8";
+
+    extraLocales = [
+      "en_US.UTF-8/UTF-8"
+      "ru_RU.UTF-8/UTF-8"
+      "uz_UZ.UTF-8/UTF-8"
+    ];
+  };
+
+  # Don't ask for password
+  security = {
+    rtkit.enable = true;
+    sudo.wheelNeedsPassword = false;
+    sudo-rs.wheelNeedsPassword = false;
+  };
+
+  services = {
+    # Sensors
+    libinput.enable = true;
+    # Use iGPU for graphics
+    xserver.videoDrivers = ["intel"];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
---
+++ hosts/arm64-linux/parallels/disk-configuration.nix
@@ -0,0 +1,41 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            BOOT = {
+              size = "2G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            SWAP = {
+              size = "14G";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true;
+              };
+            };
+            ROOT = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
